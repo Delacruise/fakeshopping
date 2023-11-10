e17caf60752b02bca06fcac43394796e44b5ddb6
Luke Daffue <25479590+Delacruise@users.noreply.github.com>
2023-11-10 17:10:07 +0400

    WIP(footer): Implement styling Style: style footer section

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,7 +1,41 @@
+import Image from 'next/image';
+
 export default function Footer() {
   return (
-    <div className='footer w-full bg-indigo-900 p-4 text-center text-white text-3xl mb-4'>
-      Footer
-    </div>
+    <footer className='footerContainer w-full p-4 text-center text-white text-3xl flex gap-10 justify-between'>
+      <div className='column p-4'>
+        <Image src='/logo.png' alt='Logo' width={150} height={150} />
+        <p className='text-sm tracking-widest text-left mt-4 uppercase text-gray-400 font-semibold'>
+          Old Crow Fakes
+        </p>
+        <div className='socialIcons'>
+          <div className='icon'>x</div>
+          <div className='icon'>f</div>
+          <div className='icon'>l</div>
+          <div className='icon'>i</div>
+        </div>
+      </div>
+      <div className='column p-4'>
+        <p className='footerTitle '>Quick Links</p>
+        <p className='footerLink '>Dummy Link</p>
+        <p className='footerLink'>Dummy Link</p>
+        <p className='footerLink'>Dummy Link</p>
+        <p className='footerLink'>Dummy Link</p>
+      </div>
+      <div className='column p-4'>
+        <p className='footerTitle '>important links</p>
+        <p className='footerLink '>Dummy Link</p>
+        <p className='footerLink'>Dummy Link</p>
+        <p className='footerLink'>Dummy Link</p>
+        <p className='footerLink'>Dummy Link</p>
+      </div>
+      <div className='column p-4'>
+        <p className='footerTitle '>Get in touch</p>
+        <p className='text-gray-400 font-normal text-lg text-left'>
+          Some important in formation about getting touch with the owners of
+          this fake one line business
+        </p>
+      </div>
+    </footer>
   );
 }
