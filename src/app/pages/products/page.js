b5a7b0470b42cc886b09e713e5c1aeb2fbf7e794
Luke Daffue <25479590+Delacruise@users.noreply.github.com>
2023-11-13 15:01:38 +0400

    WIP(products) UI layout products page style: implement layout styles for products page

--- src/app/pages/products/page.js
+++ src/app/pages/products/page.js
@@ -1,5 +1,34 @@
-import React from 'react'
+import Filter from '../../components/filter'
+import SearchBar from '../../components/SearchBar';
+import Categories from '../../components/productCategories';
 
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <div className='pageContainer flex gap-4'>
+      <div className='leftCol '>
+        <div className='containerBlocks'>
+          <div className='blockTitle'>Filter</div>
+          <Filter />
+        </div>
+        <div className='containerBlocks'>
+          <div className='blockTitle'>categories</div>
+          <Categories />
+        </div>
+      </div>
+      <div className='rightCol'>
+        <div className='containerBlocks'>
+          <SearchBar />
+        </div>
+        <div className='containerBlocks'>
+          <div className='blockTitle'>Results</div>
+          <div className='no__Results'>
+            No results fond for search query: " "
+          </div>
+          <div className='results__Found'>
+            Show results
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
